65030bbf12b19e046a1e0d6a6031248a59d994f2
Sujay Ghosh <sujayg222@gmail.com>
2025-07-27 21:23:55 +0530

    Changes

---
+++ backend/src/lib/Quix.ts
@@ -0,0 +1,24 @@
+interface Problem {
+    title: string;
+    description : string;
+    image?: string;
+    answer: string;
+    options: {
+        id: number;
+        title: string;
+    }[]; 
+}
+
+export class Quiz  {
+    private roomId: string;
+    private hasStarted: Boolean;
+    private problems: Problem[];
+
+    constructor(roomId: string){
+        this.roomId = roomId;
+        this.hasStarted = false;
+        this.problems = [];
+    }
+
+
+}
---
+++ backend/src/manager/UserManager.ts
@@ -0,0 +1,29 @@
+import { Socket } from "socket.io";
+
+export class UserManager {
+    private users: {
+        roomId: string;
+        socket:Socket
+    }[]
+
+    constructor(){
+        this.users = [];
+    }
+
+    addUser (roomId: string, socket: Socket){
+        this.users.push({
+            roomId,
+            socket
+        });
+
+        this.createHandler(roomId, socket);
+    }
+
+    private createHandler(roomId: string, socket: Socket){
+        socket.on('submission', (data) => {
+            
+        })
+    }
+
+
+}
