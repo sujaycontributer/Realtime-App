71630da88e126e9d84a1a6c51fe375e5270c7084
Sujay Ghosh <sujayg222@gmail.com>
2025-07-28 10:41:05 +0530

    Checked the Io instance

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -2,35 +2,43 @@ import express from 'express';
 import cors from 'cors'
 import  http from 'http';
 import { Server } from 'socket.io';
+import { IoManager } from './manager/IoManager';
 
 
-const app = express();
+// const app = express();
+// const server = http.createServer(app);
+// const io = new Server(server, {
+//     cors: {
+//         origin:'http://localhost:5173'
+//     }
+// });
 
-const server = http.createServer(app);
-const io = new Server(server, {
-    cors: {
-        origin:'http://localhost:5173'
-    }
-});
 
-
-io.on('connection', (socket) => { 
+// io.on('connection', (socket) => { 
     
-    socket.broadcast.emit('New user join', `${socket.id} is just joined`);
+//     socket.broadcast.emit('New user join', `${socket.id} is just joined`);
 
-    socket.on('greetings', (data) => {
-        console.log(data.message);
-    })
+//     socket.on('greetings', (data) => {
+//         console.log(data.message);
+//     })
 
-    socket.on('message', (data) => {
-        console.log(data.message, socket.id);
-        socket.to(data.toSend).emit('receive', {
-            message: data.message
-        })
-    })
+//     socket.on('message', (data) => {
+//         console.log(data.message, socket.id);
+//         socket.to(data.toSend).emit('receive', {
+//             message: data.message
+//         })
+//     })
 
 
-})
+// })
+
+const io = IoManager.getIo();
+io.listen(3000);
+
+io.on('connection', (socket) => {
+    // 3 admin events
+    // 2 client events
+});
 
 
 
