adf5193175a9fa822b8f5b37c16896f3cb24cf24
Sujay Ghosh <141671791+sujaycontributer@users.noreply.github.com>
2025-08-27 22:27:55 +0530

    Update ClinetLobby.tsx

--- client/src/components/ClinetLobby.tsx
+++ client/src/components/ClinetLobby.tsx
@@ -48,8 +48,6 @@ export default function ClinetLobby() {
     }, []);
 
 
-
-
   return (
     <div  className="min-h-screen fixed top-0 left-0 md:left-[305px] w-full flex flex-col justify-center items-center bg-gray-100">
       <div className={`${!problem ? "block": "hidden"} md:mr-[400px] flex justify-center`}><LoaderFive  text="Wait for admin to start ..."/></div>
