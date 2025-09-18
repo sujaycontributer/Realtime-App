765ae16592d01df7caba6f29afc2c6fdd875859e
Sujay Ghosh <141671791+sujaycontributer@users.noreply.github.com>
2025-09-18 23:58:32 +0530

    Update SidebarMemo.tsx

--- client/src/components/SidebarMemo.tsx
+++ client/src/components/SidebarMemo.tsx
@@ -53,7 +53,7 @@ export function SidebarDemo() {
     <div
       className={cn(
         "mx-auto flex w-full flex-1 flex-col overflow-hidden rounded-md border border-neutral-200 bg-gray-100 md:flex-row dark:border-neutral-700 dark:bg-neutral-800",
-        "h-screen", // for your use case, use `h-screen` instead of `h-[60vh]`
+        "h-screen", // for  better use case, use `h-screen` instead of `h-[60vh]`
       )}
     >
       <Sidebar open={open} setOpen={setOpen} animate={false}>
