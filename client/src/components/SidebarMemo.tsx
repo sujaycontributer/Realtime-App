70aab67845e73dea21884a541945a63427fc7d60
Sujay Ghosh <141671791+sujaycontributer@users.noreply.github.com>
2025-09-18 23:58:02 +0530

    Update SidebarMemo.tsx

--- client/src/components/SidebarMemo.tsx
+++ client/src/components/SidebarMemo.tsx
@@ -118,14 +118,3 @@ export const LogoIcon = () => {
     </a>
   );
 };
-
-// Dummy dashboard component with content
-// const Dashboard = () => {
-//   return (
-//     <div className="flex flex-1">
-//       <div className="flex h-full w-full flex-1 flex-col gap-2 rounded-tl-2xl border border-neutral-200 bg-red-200 p-2 md:p-10 dark:border-neutral-700 dark:bg-neutral-900">
-          
-//       </div>
-//     </div>
-//   );
-// };
